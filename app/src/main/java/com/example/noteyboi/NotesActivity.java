6872454a8756b643d7dd9de6e5f840b4498d82e0
Ron-TheDev <24922476+Ron-TheDev@users.noreply.github.com>
2019-07-26 16:14:29 -0400

    Ver 4.5 - Animations

--- app/src/main/java/com/example/noteyboi/NotesActivity.java
+++ app/src/main/java/com/example/noteyboi/NotesActivity.java
@@ -2,50 +2,61 @@ package com.example.noteyboi;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class NotesActivity extends AppCompatActivity {
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    int noteId = -1;
-    private TextView nameview;
-    private TextView noteview;
-////////////////////////////////////////////////////////////////////////////////////////////////////
 
+public class NotesActivity extends AppCompatActivity {
+    int noteId = -1;
+    private TextView nameview, noteview;
+    private Animation fab_open,fab_close;
+    boolean open = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
         getIncomingIntentEdit();
         nameview = findViewById(R.id.NoteName);
         noteview = findViewById(R.id.Notes);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
 
 
-        FloatingActionButton fab = findViewById(R.id.fabsave);
+        final FloatingActionButton fab = findViewById(R.id.fabsave);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Saved", Snackbar.LENGTH_LONG)
-                        .setAction("??", null).show();
                 String textname =  nameview.getText().toString();
                 String textnote =  noteview.getText().toString();
+
+                Snackbar.make(view, "Saved", Snackbar.LENGTH_LONG)
+                        .setAction("??", null).show();
+
+                if (noteId <= -1) {
+                    MainActivity.mNoteNames.add("");
+                    MainActivity.mNotes.add("");
+                    noteId= MainActivity.mNoteNames.size() - 1;
+                }
+
                 MainActivity.mNoteNames.set(noteId, textname);
                 MainActivity.mNotes.set(noteId, textnote);
                 MainActivity.adapter.notifyDataSetChanged();
-                Log.d("Debug", "onTextChanged: Click");
+                fab.startAnimation(fab_close);
+                fab.hide();
+                open = false;
             }
         });
-////////////////////////////////////////////////////////////////////////////////////////////////////
     }
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
     private void getIncomingIntentEdit(){
         if (getIntent().hasExtra("note_name") && getIntent().hasExtra("note_desc")) {
             String noteName = getIntent().getStringExtra("note_name");
@@ -62,16 +73,44 @@ public class NotesActivity extends AppCompatActivity {
         TextView desc = findViewById(R.id.Notes);
         name.setText(Name);
         desc.setText(Desc);
+        final FloatingActionButton fabsave = findViewById(R.id.fabsave);
 
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
-        if (noteId != -1){
-            Log.d("Loaded", "onTextChanged: Right id");
-        }else{
-            MainActivity.mNoteNames.add("");
-            MainActivity.mNotes.add("");
-            noteId= MainActivity.mNoteNames.size() - 1;
-            MainActivity.adapter.notifyDataSetChanged();
-        }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!open){
+                    fabsave.startAnimation(fab_open);
+                    fabsave.show();
+                    open = true;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        desc.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!open){
+                    fabsave.startAnimation(fab_open);
+                    fabsave.show();
+                    open = true;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 }
+///if(text.equals("")){}
+
--- app/src/main/java/com/example/noteyboi/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/noteyboi/RecyclerViewAdapter.java
@@ -3,19 +3,16 @@ package com.example.noteyboi;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
-import java.util.HashSet;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
